63b343a6169ace991132826ae56ff87cf48b50e8
srini-avoz <srini.avoz@gmail.com>
2021-04-24 20:21:53 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
 import styles from '../styles/Home.module.css'
+import Image from 'next/image'
 
 export default function Home() {
   return (
       <div className="mysvg">
-      <script 
-        type="text/javascript"
-        src="/static/greeting.js">
-        
-      </script>
+      <Image
+        src="/static/birthday.png
+        "
+        alt="Sumanth"
+        width={800}
+        height={800}
+      />
+      
     </div>
   )
 }
